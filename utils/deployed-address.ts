89382de21d70c786d81b81b2d40de17556355ea5
vanthanhzc3 vaz <vanthanhng5628@gmail.com>
2024-08-16 07:11:22 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x3BbB3F44999FDb9121a0b992CB837e70BEdDD750'
+export const ProxyAdmin = '0xE3B79f269D77a8875A44ef82Dc8d65cE6d6a69cE'
+export const SWTRImplementation = '0x95bA37e6723d2CcB22b18b593693C29Da2c2EE62'
     
